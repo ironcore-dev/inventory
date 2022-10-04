ac83d30c65f3cf79ce6247e69a7ca296b47526c8
Florin Peter <public@florin-peter.de>
2022-10-04 15:36:16 +0200

    cleanup database config struct

--- pkg/redis/database_config.go
+++ pkg/redis/database_config.go
@@ -2,81 +2,27 @@ package redis
 
 type DatabaseConfig struct {
 	DATABASES struct {
-		APPLDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"APPL_DB"`
-		ASICDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"ASIC_DB"`
-		COUNTERSDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"COUNTERS_DB"`
-		LOGLEVELDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"LOGLEVEL_DB"`
-		CONFIGDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"CONFIG_DB"`
-		PFCWDDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"PFC_WD_DB"`
-		FLEXCOUNTERDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"FLEX_COUNTER_DB"`
-		STATEDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"STATE_DB"`
-		SNMPOVERLAYDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"SNMP_OVERLAY_DB"`
-		ERRORDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"ERROR_DB"`
-		RESTAPIDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"RESTAPI_DB"`
-		GBASICDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"GB_ASIC_DB"`
-		GBCOUNTERSDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"GB_COUNTERS_DB"`
-		GBFLEXCOUNTERDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"GB_FLEX_COUNTER_DB"`
-		EVENTDB struct {
-			ID        int    `json:"id"`
-			Separator string `json:"separator"`
-			Instance  string `json:"instance"`
-		} `json:"EVENT_DB"`
+		APPLDB          Database `json:"APPL_DB"`
+		ASICDB          Database `json:"ASIC_DB"`
+		COUNTERSDB      Database `json:"COUNTERS_DB"`
+		LOGLEVELDB      Database `json:"LOGLEVEL_DB"`
+		CONFIGDB        Database `json:"CONFIG_DB"`
+		PFCWDDB         Database `json:"PFC_WD_DB"`
+		FLEXCOUNTERDB   Database `json:"FLEX_COUNTER_DB"`
+		STATEDB         Database `json:"STATE_DB"`
+		SNMPOVERLAYDB   Database `json:"SNMP_OVERLAY_DB"`
+		ERRORDB         Database `json:"ERROR_DB"`
+		RESTAPIDB       Database `json:"RESTAPI_DB"`
+		GBASICDB        Database `json:"GB_ASIC_DB"`
+		GBCOUNTERSDB    Database `json:"GB_COUNTERS_DB"`
+		GBFLEXCOUNTERDB Database `json:"GB_FLEX_COUNTER_DB"`
+		EVENTDB         Database `json:"EVENT_DB"`
 	} `json:"DATABASES"`
 	VERSION string `json:"VERSION"`
+}
+
+type Database struct {
+	ID        int    `json:"id"`
+	Separator string `json:"separator"`
+	Instance  string `json:"instance"`
 }
